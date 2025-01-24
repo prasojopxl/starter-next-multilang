fdf3b2388bc927818b08fab6241449671eaad0f9
prasojopxl <prasojodesigner@gmail.com>
2025-01-25 00:03:36 +0700

    add time hydration warning

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,9 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
-
 export default async function RootLayout({children, params }: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
     const { locale } = await params;
     return(
     <html lang={locale}>
         <body>
+            <time dateTime={Date.now().toString()} suppressHydrationWarning />
             {children}
         </body>
     </html>
