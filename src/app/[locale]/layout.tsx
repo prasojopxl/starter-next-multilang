c28ba98745da30ba722f555979da5d487519d113
prasojopxl <prasojodesigner@gmail.com>
2025-01-24 23:57:42 +0700

    fixing lang next15

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,32 +1,16 @@
-    import {NextIntlClientProvider} from 'next-intl';
-    import {getMessages} from 'next-intl/server';
-    import {notFound} from 'next/navigation';
-    import {routing} from '@/i18n/routing';
-    
-    export default async function LocaleLayout({
-        children,
-        params: {locale}
-    }: {
-        children: React.ReactNode;
-        params: {locale: string};
-    }) {
-        // Ensure that the incoming `locale` is valid
-        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-        if (!routing.locales.includes(locale as any)) {
-            notFound();
-        }
-    
-        // Providing all messages to the client
-        // side is the easiest way to get started
-        const messages = await getMessages();
-    
-        return (
-        <html lang={locale}>
-            <body>
-            <NextIntlClientProvider messages={messages}>
-                {children}
-            </NextIntlClientProvider>
-            </body>
-        </html>
-        );
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+
+export default async function RootLayout({children, params }: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
+    const { locale } = await params;
+    return(
+    <html lang={locale}>
+        <body>
+            {children}
+        </body>
+    </html>
+
+    )
     }
