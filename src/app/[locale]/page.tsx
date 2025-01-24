ebf6920443c68a360dae3f82334699db280ffcef
prasojopxl <prasojodesigner@gmail.com>
2025-01-25 02:27:27 +0700

    config talwind 4.x and shadcnui with next15

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,19 @@
 import {useTranslations} from 'next-intl';
 import {Link} from '@/i18n/routing';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 
 export default function HomePage() {
     const t = useTranslations('HomePage');
     return (
         <div className="bg-red-500">
+
             <h1>{t('title')}</h1>
             <Link href="/about">{t('about')}</Link>
+            <div>
+                <Button variant={"outline"}>Click me</Button>
+                <Input />
+            </div>
         </div>
     );
 }
